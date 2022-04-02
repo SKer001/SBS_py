f1e0bfb9d917c7d5fd247273cb7d1b8df249e513
SKer001 <ppappapp8077616@gmail.com>
2022-04-02 22:48:20 +0800

    Create 4if.py

---
+++ 4if.py
@@ -0,0 +1,49 @@
+#用法
+#  if [變數](==,>,<,>=,<=)(數值,字串,布林值,變數):
+#  elif [變數](==,>,<,>=,<=)(數值,字串,布林值,變數):     
+#  else:
+#
+#
+#
+#數字
+G = 10
+if G>9:
+    print("昏迷")
+elif G>-3:
+    print("正常")
+elif G == 1:
+    print("原始")
+else:
+    print("昏迷")
+
+#字串
+day = "rainy"
+if day=="rainy":
+    print("下雨")
+elif day=="clear":
+    print("晴天")
+elif day=="dark":
+    print("陰天")
+
+#布林值
+TF1 = False
+if TF1:
+    print(1)
+else:
+    print(0)
+
+#and  且
+TF1 = True
+TF2 = False
+if TF1 and TF2:
+    print(1)
+else:
+    print(0)
+
+#or 或
+TF1 = True
+TF2 = False
+if TF1 or TF2:
+    print(1)
+else:
+    print(0)
