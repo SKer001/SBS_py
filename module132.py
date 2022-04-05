3ef373a0ded63a8135a2b4af3215267e0e3dc76e
SKer001 <ppappapp8077616@gmail.com>
2022-04-05 13:29:04 +0800

    Create module132.py

---
+++ module132.py
@@ -0,0 +1,10 @@
+class me:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def yourname(self):
+        print(self.name)
+        
+    def yourage(self):
+        print(self.age)
