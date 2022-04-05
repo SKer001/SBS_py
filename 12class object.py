a0e16328dc006938bbd9de3c03afb628519e4a26
SKer001 <ppappapp8077616@gmail.com>
2022-04-05 12:02:58 +0800

    Create 12class object.py

---
+++ 12class object.py
@@ -0,0 +1,22 @@
+#class 名字:
+#    def __init__(self,變數1,變數2):
+#        self.變數1 = 變數1
+#        self.變數2 = 變數2
+
+
+#class相當於一個自定義的變數
+class aboutme:
+    def __init__(self,name,age,where):
+        self.N = name
+        self.A = age
+        self.W = where
+
+N = input("你的名字: ")
+A = input("你的年齡: ")
+W = input("你哪裡人: ")
+#myself包含aboutme的三種變數
+myself = aboutme(N,A,W)
+
+print("我是 "+myself.N +"\n"+"我 "+myself.A +" 歲"+"\n"+"我來自 "+myself.W)
+with open("./file/12 namecard.txt",mode="w",encoding='utf-8') as File:
+    File.write("我是 "+myself.N +"\n"+"我 "+myself.A +" 歲"+"\n"+"我來自 "+myself.W)
