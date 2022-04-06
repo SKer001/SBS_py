2c1b69ef5b953e916eb4c0304277cd24ecba5866
SKer001 <ppappapp8077616@gmail.com>
2022-04-06 20:41:38 +0800

    Create 計算機.py

---
+++ game/計算機.py
@@ -0,0 +1,23 @@
+N1 = float(input("第一數: "))
+N2 = float(input("第二數: "))
+OP = input("運算符號 + - * /: ")
+
+while S==1:
+    if OP=="+":
+        print(N1 + N2)
+    elif OP=="-":
+        print(N1 - N2)
+    elif OP=="*":
+        print(N1 * N2)
+    elif OP=="/":
+        print(N1 / N2)
+    
+    print("是否結束?(0 or 1):")
+    S = float(input(":"))
+    
+    if S>0:
+        N1 = float(input("第一數: "))
+        N2 = float(input("第二數: "))
+        OP = input("運算符號 + - * /: ")
+
+print("結束")
